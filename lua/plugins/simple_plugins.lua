e65cd26d0f6b1a0b03caaa0f03fe4d6d797297c2
CorvusPrudens <corvusprudens@gmail.com>
2024-10-19 17:50:09 -0600

    Moved to bacon-ls

--- lua/plugins/simple_plugins.lua
+++ lua/plugins/simple_plugins.lua
@@ -16,4 +16,65 @@ return {
     event = "LspAttach",
     config = true,
   },
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = true,
+    keys = {
+      "<leader>wt",
+      "<cmd>ToggleTerm size=20<cr>",
+      desc = "Open a horizontal terminal",
+    },
+  },
+  -- {
+  --   "mrcjkb/rustaceanvim",
+  --   version = "^4", -- Recommended
+  --   ft = { "rust" },
+  --   opts = {
+  --     tools = {
+  --       enable_clippy = false,
+  --     },
+  --     server = {
+  --       on_attach = function(_, bufnr)
+  --         vim.keymap.set("n", "<leader>cR", function()
+  --           vim.cmd.RustLsp("codeAction")
+  --         end, { desc = "Code Action", buffer = bufnr })
+  --         vim.keymap.set("n", "<leader>dr", function()
+  --           vim.cmd.RustLsp("debuggables")
+  --         end, { desc = "Rust Debuggables", buffer = bufnr })
+  --       end,
+  --       default_settings = {
+  --         -- rust-analyzer language server configuration
+  --         ["rust-analyzer"] = {
+  --           cargo = {
+  --             allFeatures = false,
+  --             loadOutDirsFromCheck = true,
+  --             buildScripts = {
+  --               enable = true,
+  --             },
+  --           },
+  --           -- Add clippy lints for Rust.
+  --           checkOnSave = true,
+  --           procMacro = {
+  --             enable = true,
+  --             ignored = {
+  --               ["async-trait"] = { "async_trait" },
+  --               ["napi-derive"] = { "napi" },
+  --               ["async-recursion"] = { "async_recursion" },
+  --             },
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+  --     if vim.fn.executable("rust-analyzer") == 0 then
+  --       LazyVim.error(
+  --         "**rust-analyzer** not found in PATH, please install it.\nhttps://rust-analyzer.github.io/",
+  --         { title = "rustaceanvim" }
+  --       )
+  --     end
+  --   end,
+  -- },
 }
